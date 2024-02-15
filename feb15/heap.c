53a2e19b7b9f599ed26ac049be5ecb2eec53cea0
John Hightower <john.hightower@uafs.edu>
2024-02-15 15:30:25 -0600

    Update

--- feb15/heap.c
+++ feb15/heap.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int main(int argc, char *argv()){
+    char *str;
 
-int main(int argc, char *argv){
+    str = malloc(sizeof(char) * 10);
+    if(str==NULL){
+        exit(1);
+    }
+   strcpy(str,"Hello");
+   printf("%s\n",str);
+
+   free(str);
 
     return 0;
 }
